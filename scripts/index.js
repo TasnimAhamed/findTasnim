cb5b166eb5147204caf16e36fb9befe2f78b04c2
TasnimAhmed <tasnim.ahamed.20@gmail.com>
2021-05-03 02:42:31 +0600

    another commit

--- scripts/index.js
+++ scripts/index.js
@@ -180,20 +180,22 @@ $(function() {
   }, 10);
 
   // CONTACT FORM
-  $('#contact-form').submit(function(e) {
-    e.preventDefault();
-    $.ajax({
-      url: 'https://formspree.io/f/xbjqdkbr',
-      method: 'POST'      
-    }).done(function(response) {
-      $('#success').addClass('expand');
-      $('#contact-form')
-        .find('input[type=text], input[type=email], textarea')
-        .val('');
-    });
-  });
+  // $('#contact-form').submit(function(e) {
+  //   e.preventDefault();
+  //   $.ajax({
+  //     url: 'https://formspree.io/f/xbjqdkbr',
+  //     method: 'POST',
+  //     data: { message: $('form').serialize() },
+  //     dataType: 'json'      
+  //   }).done(function(response) {
+  //     $('#success').addClass('expand');
+  //     $('#contact-form')
+  //       .find('input[type=text], input[type=email], textarea')
+  //       .val('');
+  //   });
+  // });
 
-  $('#close').click(function() {
-    $('#success').removeClass('expand');
-  });
+  // $('#close').click(function() {
+  //   $('#success').removeClass('expand');
+  // });
 });
